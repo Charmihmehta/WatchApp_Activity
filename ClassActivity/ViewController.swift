f1862a4830e1bbeb386d3fcbf99695ec881908fc
Charmi Mehta <charmimehta@Charmis-MacBook-Pro.local>
2019-03-07 09:59:32 -0500

    First commit

---
+++ ClassActivity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClassActivity
+//
+//  Created by Charmi Mehta on 2019-03-07.
+//  Copyright © 2019 Charmi Mehta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
