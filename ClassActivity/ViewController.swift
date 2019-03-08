f997d3a8e8328652eab319c61c97b9845549bb3c
Charmi Mehta <charmimehta@charmis-macbook-pro.local>
2019-03-07 22:22:34 -0500

    Adding buttons

--- ClassActivity/ViewController.swift
+++ ClassActivity/ViewController.swift
@@ -3,7 +3,7 @@
 //  ClassActivity
 //
 //  Created by Charmi Mehta on 2019-03-07.
-//  Copyright © 2019 Charmi Mehta. All rights reserved. jkjk
+//  Copyright © 2019 Charmi Mehta. All rights reserved.
 //
 
 import UIKit
@@ -16,6 +16,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var subscribeBtn: UIButton!
     var wcSession : WCSession!
     
+  
+    let list = ["Germany-China" ,"Spain-South Africa","Norway-Nigeria" , "Austraila-Italy" , "Brazil-Jamaica" , "England-Scotland"]
+    let date = ["08 June 2019" , "08 June 2019" , "08 June 2019" , "09 June 2019", "09 June 2019", "09 June 2019"]
     
     
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
@@ -40,34 +43,47 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         wcSession = WCSession.default
         wcSession.delegate = self
         wcSession.activate()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.tableView.reloadData()
     }
     
-    let list = ["Germany-China" ,"Spain-South Africa","Norway-Nigeria" , "Austraila-Italy" , "Brazil-Jamaica" , "England-Scotland"]
-    let date = ["08 June 2019" , "08 June 2019" , "08 June 2019" , "09 June 2019", "09 June 2019", "09 June 2019"]
+   
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
             return list.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "cell")
-        if cell == nil {
-            cell = UITableViewCell(style: UITableViewCell.CellStyle.value1, reuseIdentifier: "cell")
-        }
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+      // var cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "cell") as! TableViewCell
+//        if cell == nil {
+//            cell = UITableViewCell(style: UITableViewCell.CellStyle.value1, reuseIdentifier: "cell") as! TableViewCell
+//        }
         cell.textLabel?.text = list[indexPath.row]
         cell.detailTextLabel?.text = date[indexPath.row]
-        return(cell)
+    
+        cell.subscribeBtn.tag = indexPath.row
+        cell.subscribeBtn.addTarget(self, action: "BtnAction:", for: .allEvents)
+        
+        return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("You selected cell #\(indexPath.row)!")
     }
     
-    
+    @IBAction func BtnAction(_ sender: UIButton){
+        print("You have clicked subscribe to the notification")
+        let no = sender.tag
+        print(no)
+    }
 
     @IBAction func btnClick(_ sender: Any) {
        if(subscribeBtn.titleLabel?.text == "Subscribe")
        {
          print("You have subscribe to the notification")
-            subscribeBtn.setTitle("UnSubscribe", for: .normal)
+          NSLog("%@", "button 1")
+          subscribeBtn.setTitle("UnSubscribe", for: .normal)
         
         let message = ["message" : "message sent"]
         do {
@@ -79,6 +95,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
        else if (subscribeBtn.titleLabel?.text == "UnSubscribe"){
           print("You have Not subcribe to the notification")
+        NSLog("%@", "button 2")
             subscribeBtn.setTitle("Subscribe", for: .normal)
        
         }
